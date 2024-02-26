857da79b79094bf251bbe9b44c946f920364e976
radim <radimkotajny@gmail.com>
2024-02-26 15:35:24 +0100

    working on maturitni prace detailing

--- src/app/vyhledane-prace/[id]/[slug]/page.tsx
+++ src/app/vyhledane-prace/[id]/[slug]/page.tsx
@@ -1,8 +1,8 @@
 'use client'
-import {Box, Button, Divider, Grid, Heading, Spinner} from "@chakra-ui/react";
+import {Box, Button, Divider, Flex, Grid, GridItem, Heading, Spinner, Text} from "@chakra-ui/react";
 import {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
-import {Image} from "@chakra-ui/next-js";
+import {Image} from "@chakra-ui/react";
 
 type APIData = {
   id: number,
@@ -12,7 +12,9 @@ type APIData = {
   predmet?: string,
   jmeno_prijmeni?: string,
   vedouci?: string,
-  Message?: string;
+  Message?: string,
+  prakticka_cast?: string,
+  obsah?: string
 };
 
 export default function Page({params}: { params: { id: number, slug: string } }) {
@@ -32,6 +34,10 @@ export default function Page({params}: { params: { id: number, slug: string } })
       }
     }
   }, [params.id]);
+
+  useEffect(() => {
+    console.log(data)
+  }, [data]);
 
 
   if (isLoading) {
@@ -60,18 +66,57 @@ export default function Page({params}: { params: { id: number, slug: string } })
            boxShadow="0 8px 32px 0 rgba( 31, 38, 135, 0.37 )"
            backdropFilter="blur( 4px )"
            borderRadius="xl"
+           justifyItems="center"
            p={8}
            m={4}>
-        <Grid gridTemplateColumns="repeat(2, 1fr)">
-          <Box boxSize='sm'>
-            <Image src='' alt='Dan Abramov' fill/>
-          </Box>
-          <Box>
-            <Heading>{data.tema ?? "Chybí název"}</Heading>
-            <Divider/></Box>
-          <Box></Box>
-        </Grid>
+        <Grid
+          h="full"
+          justifyContent="center"
+          alignItems="center"
+          gap={5}
+          gridTemplateColumns={{base: "repeat(1, 1fr)", xl: "repeat(2, 1fr)"}}>
+          <GridItem boxSize='400px'>
+            <Box boxSize='500px'>
+              <Image
+                m={1}
+                borderRadius="xl"
+                src='https://media.istockphoto.com/id/185244309/photo/work-in-progress.jpg?s=612x612&w=0&k=20&c=ECQ9StWhFTfFjWg_yt8ITEyAuZpRLVV8akD20jagc-Y='
+                alt='Random image from stock photo'
+                objectFit="cover"
+              />
+            </Box>
+          </GridItem>
+          <GridItem boxSize="lg" justifySelf="center" h="full">
+            <Box>
+              <Heading fontSize="3xl">{data.tema ?? "Chybí název"}</Heading>
+              <Divider/>
+            </Box>
 
+            <Flex flexDir="row" justifyContent="space-between" w="full" my={1}>
+              <Text fontWeight="200">Autor: {data.jmeno_prijmeni}</Text>
+              <Text fontWeight="200">Vedoucí: {data.vedouci}</Text>
+            </Flex>
+
+            <Flex flexDir="row" justifyContent="space-between" w="full" my={1}>
+              <Text fontWeight="200">{data.skolni_rok}</Text>
+              <Text fontWeight="200">Obor:&nbsp;
+                <span style={{textTransform: "uppercase"}}>{data.obor}</span>
+              </Text>
+            </Flex>
+            <Divider my={2}/>
+            <Flex flexDir="column">
+              <Box>
+                <Text>Praktická část</Text>
+                <Text fontWeight="200">{data.prakticka_cast}</Text>
+              </Box>
+              <Divider my={2}/>
+              <Box>
+                <Text>Obsah práce</Text>
+                <Text fontWeight="200">{data.obsah}</Text>
+              </Box>
+            </Flex>
+          </GridItem>
+        </Grid>
       </Box>
     </Box>
   );
