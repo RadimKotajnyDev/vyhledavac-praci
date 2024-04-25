75b3edd259b61d814d39a0f627cc213f09d64d73
Radim Kotajny <radimkotajny@gmail.com>
2024-04-25 10:52:44 +0200

    removed unused Logout.tsx

--- src/app/components/Logout.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import {logout} from "@/app/login/actions";
-import {Button} from "@chakra-ui/react";
-
-export const Logout = () => {
-  return (
-    <Button onClick={() => logout()}>logout</Button>
-  )
-}
